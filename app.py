e02c81c724dff3f0de11fedf8ba4899c673010aa
Big <bigmorning@live.com>
2018-05-17 22:27:32 +0700

    change kam to word

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def handle_message(event):
     conn.commit()
     
     try:
-        cur.execute("CREATE TABLE pocha (kam text) AS SELECT DISTINCT kam FROM inputmes;")
+        cur.execute("CREATE TABLE pocha (kam text) AS SELECT DISTINCT word FROM inputmes;")
     except psycopg2.ProgrammingError:
         conn.rollback()
 
