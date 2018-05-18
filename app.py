6db965a23fd1ca9a74580ad037a82731ed84cd53
Big <bigmorning@live.com>
2018-05-18 16:44:39 +0700

    แบ่งการ connect เป็น 2

--- app.py
+++ app.py
@@ -48,37 +48,41 @@ def handle_message(event):
     
     DATABASE_URL = os.environ['DATABASE_URL']
     
-    try:
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    except:
-        print("I am unable to connect to the database")
-    
-    cur = conn.cursor()
+    def connect():
+        try:
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        except:
+            print("I am unable to connect to the database")
 
-    try:
-        cur.execute("CREATE TABLE inputmes (word text);")
-    except psycopg2.ProgrammingError:
-        conn.rollback()
-    cur.execute("INSERT INTO inputmes VALUES (%(str)s);", {'str':a})
-    conn.commit()
+        cur = conn.cursor()
     
-    try:
-        cur.execute("CREATE TABLE pocha (kam text, prapet INT);")
-    except psycopg2.ProgrammingError:
-        conn.rollback()
-    cur.execute("INSERT INTO pocha SELECT DISTINCT word FROM inputmes;")
-    conn.commit()
-    
-    #delete duplicate record using code from https://stackoverflow.com/questions/6583916/delete-duplicate-records-in-postgresql
-    cur.execute("DELETE FROM pocha a USING (SELECT MIN(ctid) as ctid, kam FROM pocha GROUP BY kam HAVING COUNT(*) > 1) b WHERE a.kam = b.kam AND a.ctid <> b.ctid;")
-    conn.commit()
-    
-    m = cur.fetchall()
-    for rows in m:
-        print(rows)
+    def inputmes():
+        connect()
+        try:
+            cur.execute("CREATE TABLE inputmes (word text);")
+        except psycopg2.ProgrammingError:
+            conn.rollback()
+        cur.execute("INSERT INTO inputmes VALUES (%(str)s);", {'str':a})
+        conn.commit()
         
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+    
+    def pocha():
+        connect()
+        try:
+            cur.execute("CREATE TABLE pocha (kam text, prapet INT);")
+        except psycopg2.ProgrammingError:
+            conn.rollback()
+        cur.execute("INSERT INTO pocha SELECT DISTINCT word FROM inputmes;")
+        conn.commit()
+    
+        #delete duplicate record using code from https://stackoverflow.com/questions/6583916/delete-duplicate-records-in-postgresql
+        cur.execute("DELETE FROM pocha a USING (SELECT MIN(ctid) as ctid, kam FROM pocha GROUP BY kam HAVING COUNT(*) > 1) b WHERE a.kam = b.kam AND a.ctid <> b.ctid;")
+        conn.commit()
+    
+        cur.close()
+        conn.close()
 
 if __name__ == "__main__":
     app.run()
