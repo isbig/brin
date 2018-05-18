06f59b1aa596d3d5a2b51e95287091e3bc791a43
Big <bigmorning@live.com>
2018-05-19 01:59:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def handle_message(event):
     inputmes()
     pocha()
     s = out()
-    i = random.choice(s)
+    i = random.shuffle(s)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="คำว่า ความ" + i + " แปลกหรือไม่"))
