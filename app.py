ba6f5e7d81857f6fbb8666cdece4797e0810b6d3
Big <bigmorning@live.com>
2018-05-18 18:14:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,9 +44,6 @@ import psycopg2
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     a = event.message.text
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
     
     DATABASE_URL = os.environ['DATABASE_URL']
     
